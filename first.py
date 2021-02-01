fd51a01ff99c82c98db559e8d4ed8c5b7e73d60a
70001511952020 <heriyanto.sitorus@brilife.co.id>
2021-02-01 10:22:50 +0700

    operasi

--- first.py
+++ first.py
@@ -1,13 +1,40 @@
-print("hello world")
-print(type("hello world"))
-print(123)
-print(type(123))
-print(1.4)
-print(type(1.4))
-print(True)
-print(type(True))
+# print("hello world")
+# print(type("hello world"))
+# print(123)
+# print(type(123))
+# print(1.4)
+# print(type(1.4))
+# print(True)
+# print(type(True))
 
-x = 123
+# a = 123
 
-print(x)
-print(type(x))
+# print(a)
+# print(type(a))
+
+x = 1
+y = 2
+
+tambah = x+y
+kurang = x-y
+kali = x*y
+bagi = x/y
+ #untuk integer
+bagiInt = x//y 
+
+pangkat = x**y
+modulus = x%y
+
+print(tambah)
+print(kurang)
+print(kali)
+print(bagi)
+print(bagiInt)
+print(modulus)
+print(pangkat)
+
+a =3.17
+b = 5
+c = 7
+hitung = a * b -(c-b) + (5**7)
+print (hitung)
